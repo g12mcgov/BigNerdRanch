32d8f60582092317573000170686d7d7ee34528f
Grant McGovern <grant@cobrain.com>
2015-03-22 18:14:56 -0400

    First commit

---
+++ Quiz/Quiz/BNRQuizViewController.m
@@ -0,0 +1,40 @@
+//
+//  BNRQuizViewController.m
+//  Quiz
+//
+//  Created by Grant McGovern on 3/22/15.
+//  Copyright (c) 2015 Big Nerd Ranch. All rights reserved.
+//
+
+#import "BNRQuizViewController.h"
+
+@interface BNRQuizViewController ()
+
+/*
+ *
+ * THESE CONTAIN AlL THE VARIABLES.
+ *
+ */
+
+@property (nonatomic, weak) IBOutlet UILabel *questionLabel;
+@property (nonatomic, weak) IBOutlet UILabel *answerLabel;
+
+@end
+
+/*
+ *
+ *  THESE CONTAIN ALL THE METHODS.
+ *
+ */
+
+@implementation BNRQuizViewController
+
+- (IBAction) showQuestion:(id)sender {
+    
+}
+
+- (IBAction) showAnswer:(id)sender {
+    
+}
+
+@end
---
+++ Quiz/Quiz/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Quiz
+//
+//  Created by Grant McGovern on 3/22/15.
+//  Copyright (c) 2015 Big Nerd Ranch. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
